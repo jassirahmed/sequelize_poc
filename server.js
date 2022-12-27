e42baffc3d2982df36c73f592fabb6a3924c7ad1
jassir <a.jassir@ilmux.com>
2022-12-27 18:53:28 +0530

    feat: crud function

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require("express")
+require('./src/models')
 const bodyParser = require('body-parser')
-const userCtrl = require('./controllers/userControllers')
-require('./models')
+const userCtrl = require('./src/controllers/userControllers')
+const app = express()
+const PORT = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
-app.get('/add', userCtrl.addUser)
-app.listen(3000, () => {
+
+app.get('/add', userCtrl.addUser);
+
+app.get('/users', userCtrl.getUsers);
+
+app.get('/users/:id', userCtrl.getUser);
+
+app.post('/users', userCtrl.postUser);
+
+app.delete('/users/:id', userCtrl.deleteUser);
+
+app.patch('/users/:id', userCtrl.updateUser);
+
+app.get('/query', userCtrl.queryUser);
+
+app.get('/count', userCtrl.usersCount);
+
+app.get('/finder', userCtrl.finders)
+
+app.listen(PORT, () => {
     console.log('App will run on: http://localhost:3000');
 })
---
+++ src/controllers/userControllers.js
@@ -0,0 +1,91 @@
+const db = require('../models')
+const User = db.user;
+
+const addUser = async (req, res) => {
+    const jane = await User.create({ firstName: "Jassir", lastName: 'Ahmed' });
+    console.log(jane instanceof User);
+    await jane.save();
+    console.log("Jane was saved to database");
+    console.log(jane.toJSON());
+    res.status(200).json(jane.toJSON())
+}
+
+const getUsers = async (req, res) => {
+    const data = await User.findAll({})
+    res.status(200).json({ data: data })
+}
+
+const getUser = async (req, res) => {
+    const data = await User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({ data: data })
+}
+
+const postUser = async (req, res) => {
+    const postData = req.body
+    let data;
+    if (postData.length > 1) {
+        data = await User.bulkCreate(postData)
+    } else {
+        data = await User.create(postData)
+    }
+    res.status(200).json({ data: data })
+}
+
+const deleteUser = async (req, res) => {
+    const data = await User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({ data: data })
+}
+
+const updateUser = async (req, res) => {
+    const updateData = req.body
+    const data = await User.update(updateData, {
+        where: {
+            id: req.params.id,
+
+        }
+    })
+    res.status(200).json({ data: data })
+}
+
+const usersCount = async (req, res) => {
+    const count = await User.count();
+    res.status(200).json({ data: count + 1 })
+}
+
+const queryUser = async (req, res) => {
+    const data = await User.findAll({
+        attributes: {
+            include: ['id']
+        }
+    })
+    res.status(200).json({ data: data })
+}
+
+const finders = async (req, res) => {
+    const [user, created] = await User.findOrCreate({
+        where: { firstName: 'Jassir' },
+        defaults: {
+            lastName: 'Mohd'
+        }
+    })
+    res.status(200).json({ data: user, created: created })
+}
+module.exports = {
+    addUser,
+    getUsers,
+    getUser,
+    postUser,
+    deleteUser,
+    updateUser,
+    queryUser,
+    usersCount,
+    finders
+}
